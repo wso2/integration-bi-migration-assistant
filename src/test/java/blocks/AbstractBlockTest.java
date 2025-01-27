6f8404b976e500170cf1f18353090535478e6d2f
lochana-chathura <39232462+lochana-chathura@users.noreply.github.com>
2025-01-27 18:05:18 +0530

    Add updateAssertFile support for the tests

--- src/test/java/blocks/AbstractBlockTest.java
+++ src/test/java/blocks/AbstractBlockTest.java
@@ -11,19 +11,39 @@ import static converter.Mule2BalConverter.convertToBallerina;
 
 public class AbstractBlockTest {
 
-    private static final Path RESOURCE_DIRECTORY = Path.of("src/test/resources/");
+    private static final Path RESOURCE_DIRECTORY = Path.of("src", "test", "resources");
     private static final String BLOCKS_DIRECTORY = "blocks";
     private static final String MULE_3_DIRECTORY = "mule3";
+
+    /**
+     * <b>WARNING</b>: Enabling this flag will update all the assertion files in unit tests.
+     * Should be used only if there is a bulk update that needs to be made to the test assertions.
+     */
+    private static final boolean UPDATE_ASSERTS = false;
 
     public static void testMule3ToBal(String sourcePath, String targetPath) {
         testMuleToBal(MULE_3_DIRECTORY, sourcePath, targetPath);
     }
 
     private static void testMuleToBal(String muleVersionDir, String sourcePath, String targetPath) {
-        SyntaxTree syntaxTree = convertToBallerina(RESOURCE_DIRECTORY.resolve(BLOCKS_DIRECTORY).resolve(muleVersionDir)
-                        .resolve(sourcePath).toString());
-        String expectedBalCode = getSourceText(Path.of(BLOCKS_DIRECTORY, MULE_3_DIRECTORY, targetPath));
-        Assert.assertEquals(syntaxTree.toSourceCode(), expectedBalCode);
+        Path testDir = RESOURCE_DIRECTORY.resolve(BLOCKS_DIRECTORY).resolve(muleVersionDir);
+        SyntaxTree syntaxTree = convertToBallerina(testDir.resolve(sourcePath).toString());
+        String expectedBalCode = getSourceText(testDir.resolve(targetPath));
+        String actualBalCode = syntaxTree.toSourceCode();
+        updateAssertFile(testDir.resolve(targetPath), actualBalCode);
+        Assert.assertEquals(actualBalCode, expectedBalCode);
+    }
+
+    private static void updateAssertFile(Path filePath, String newContent) {
+        if (!UPDATE_ASSERTS) {
+            return;
+        }
+
+        try {
+            Files.writeString(filePath, newContent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -34,7 +54,7 @@ public class AbstractBlockTest {
      */
     public static String getSourceText(Path sourceFilePath) {
         try {
-            return Files.readString(RESOURCE_DIRECTORY.resolve(sourceFilePath));
+            return Files.readString(sourceFilePath);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
