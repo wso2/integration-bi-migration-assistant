044c204572dda179a2f23e6d0bd182ac2c4ba6c6
hindujaB <bhinduja08@gmail.com>
2025-03-05 15:25:13 +0530

    Fix failing tests

--- src/test/java/dw/parser/DataWeaveScripts.java
+++ src/test/java/dw/parser/DataWeaveScripts.java
@@ -114,7 +114,13 @@ public class DataWeaveScripts {
             %dw 2.0
             %output application/json
             ---
-            |2021-01-01|
+            {
+                date: |2021-01-01|,
+                time: |23:59:56|,
+                timeZone: |-08:00|,
+                dateTime: |2003-10-01T23:57:59-03:00|,
+                localDateTime: |2003-10-01T23:57:59|
+            }
             """;
     public static final String SCRIPT_SIMPLE_REGEX = """
             %dw 2.0
