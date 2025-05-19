0a1a268fb1fe4afaedfc523efb66739d2e00ed99
Heshan Padamsiri <heshanp@wso2.com>
2025-05-19 12:57:16 +0530

    Add ability to convert Ballerina model to match BI (#86)
    * Implement converter
    
    * Fix imports
    
    * Fix checkstyle
    
    * Extract comments to seperate file

--- tibco/src/main/java/tibco/converter/TibcoConverter.java
+++ tibco/src/main/java/tibco/converter/TibcoConverter.java
@@ -76,6 +76,11 @@ public class TibcoConverter {
             System.exit(1);
             return;
         }
+        // TODO: enable after https://github.com/wso2-enterprise/integration-bi-migration-assistant/issues/73
+        //      BallerinaModel.Module biModule = new BICodeConverter(BICodeConverter.DEFAULT_IS_CONFIGURABLE_PREDICATE,
+        //              BICodeConverter.DEFAULT_IS_CONNECTION_PREDICATE,
+        //              (doc) -> doc.documentName().equals("utils.bal"))
+        //              .convert(result.module());
         BallerinaModel.DefaultPackage balPackage = new BallerinaModel.DefaultPackage("tibco", "sample", "0.1");
         for (BallerinaModel.TextDocument textDocument : result.module().textDocuments()) {
             try {
