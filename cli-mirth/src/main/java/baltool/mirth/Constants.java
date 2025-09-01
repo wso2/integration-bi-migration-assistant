edda442c664e701591fcb3e6d96abbaf9e5aae7c
isuruh15 <isurus@wso2.com>
2025-09-01 22:21:25 +0530

    Initial files added

---
+++ cli-mirth/src/main/java/baltool/mirth/Constants.java
@@ -0,0 +1,19 @@
+package baltool.mirth;
+
+public class Constants {
+
+    public static final String CMD_NAME = "migrate-mirth-channel";
+
+    public static final String DEFAULT_ORG_NAME = "wso2";
+    public static final String DEFAULT_PROJECT_VERSION = "0.1.0";
+
+    public static final String FILE_PATH = "filePath";
+    public static final String CONTENT = "content";
+    public static final String DEV_COPILOT_BACKEND_URL = "https://e95488c8-8511-4882-967f-ec3ae2a0f86f-dev." +
+            "e1-us-east-azure.choreoapis.dev/ballerina-copilot/ballerina-copilot-api-byo/v2.0";
+    public static final String COPILOT_BACKEND_URL = "https://dev-tools.wso2.com/ballerina-copilot/v2.0";
+    public static final int MAXIMUM_RETRY_COUNT = 2;
+
+    public static final String BALLERINA_TOML_FILE = "Ballerina.toml";
+
+}
