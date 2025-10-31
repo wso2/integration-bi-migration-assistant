cc004cb734e379187f49b6ebd0dbb55ee992d2bc
Heshan Padamsiri <hpheshan@gmail.com>
2025-10-31 15:40:07 +0530

    Extract escpe logic

--- common/src/main/java/common/ConversionUtils.java
+++ common/src/main/java/common/ConversionUtils.java
@@ -18,7 +18,13 @@
 
 package common;
 
+import java.util.regex.Pattern;
+
 public class ConversionUtils {
+
+    private static final Pattern UNESCAPED_SPECIAL_CHAR_SET = Pattern
+            .compile("([$&+,:;=\\?@#\\\\|/'\\ \\[\\}\\]<\\>.\"^*{}~`()%!-])");
+
     public static BallerinaModel.Expression.BallerinaExpression exprFrom(String expr) {
         return new BallerinaModel.Expression.BallerinaExpression(expr);
     }
@@ -29,5 +35,16 @@ public class ConversionUtils {
 
     public static BallerinaModel.TypeDesc.BallerinaType typeFrom(String type) {
         return new BallerinaModel.TypeDesc.BallerinaType(type);
+    }
+
+    /**
+     * Escapes special characters in an identifier with a preceding backslash (\). Replaces hyphens (-) with underscores
+     * (_) and escapes other special characters. This is part of making an identifier valid in Ballerina syntax.
+     *
+     * @param identifier the original identifier string
+     * @return identifier with special characters escaped and hyphens replaced
+     */
+    public static String escapeSpecialCharacters(String identifier) {
+        return UNESCAPED_SPECIAL_CHAR_SET.matcher(identifier).replaceAll("\\\\$1");
     }
 }
